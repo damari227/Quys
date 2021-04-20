0ed8629cf7d2544d4e49852bec813c812ccaab42
damari227 <gaming29zacky@gmail.com>
2021-04-20 12:56:31 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const body = document.getElementsByTagName('body')[0];
+const wrapper = document.getElementsByClassName('wrapper')[0];
+
+
+const showWrapper = () => {
+	wrapper.style.opacity = 1;
+}
